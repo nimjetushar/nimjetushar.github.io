9321ccc78d85c3d3c0bb887cd49880c189d59410
Tushar <tusharnimje21@gmail.com>
2018-10-18 22:44:27 +0530

    added build file

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,28 @@
-var gulp = require("gulp");
-var filesToMove = ["./build/index.html", "./build/favicon/**/*.*"];
+var gulp = require("gulp"),
+  del = require("del");
 
-gulp.task("default", ["move"], function() {
+var filesToMove = [
+  "./build/index.html",
+  "./build/favicon/**",
+  "./build/static/**",
+  "./build/service-worker.js"
+];
+
+var fileToDelete = [
+  "./index.html",
+  "./favicon/**",
+  "./static/**",
+  "./service-worker.js"
+];
+
+gulp.task("default", ["clean", "move"], function() {
   console.log("Moving files from build folder");
 });
 
 gulp.task("move", function() {
-  gulp.src(filesToMove, { base: "./" }).pipe(gulp.dest("./"));
+  gulp.src(filesToMove, { base: "./build" }).pipe(gulp.dest("./"));
+});
+
+gulp.task("clean", function() {
+  del(fileToDelete, { force: true });
 });
