1d51d910573c0ebe8330b5326f2d1122b1bfe2c6
Tushar Nimje <tusharnimje21@gmail.com>
2021-10-21 21:10:39 +0530

    added loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { useEffect, useState } from "react";
 import "./App.scss";
 import { Main } from "./component/main.component";
 
 function App() {
-  return (
-    <div className="application-container">
-      <Main />
-    </div>
-  );
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+      const loaderEle = document.getElementById("boot-loader");
+      if (loaderEle) {
+        loaderEle.style.display = "none";
+      }
+    }, 1000 * 5);
+  }, []);
+
+  return <div className="application-container">{loading ? "" : <Main />}</div>;
 }
 
 export default App;
