c4af3bbdb8ae1990a8b58dbe783025f2eee3d21d
Tushar Nimje <tusharnimje21@gmail.com>
2023-03-20 17:06:01 +0530

    updated build

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
