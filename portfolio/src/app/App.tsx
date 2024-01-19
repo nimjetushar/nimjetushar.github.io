a7e80a375feca4f8e82e87ba2fbc42a6610e46c7
Tushar Nimje <tusharnimje21@gmail.com>
2024-01-19 23:12:08 +0530

    updated project

---
+++ portfolio/src/app/App.tsx
@@ -0,0 +1,14 @@
+import './App.scss';
+
+export function App() {
+  return (
+    <div>
+      <h1>
+        <span> Hello there, </span>
+        Welcome portfolio 👋
+      </h1>
+    </div>
+  );
+}
+
+export default App;
--- src/config/app.constant.ts
+++
@@ -1,8 +0,0 @@
-export const NavigationConfig = {
-  home: "Home",
-  about: "About",
-  work: "Work",
-  education: "Education",
-  skill: "Skills",
-  testimonials: "testimonials",
-};
